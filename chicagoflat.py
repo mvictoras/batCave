05ed313d50f14918ccbd1652681afca1d92b8003
mvictoras <mvictoras@gmail.com>
2013-12-05 22:42:14 -0600

    Final demo version

--- chicagoflat.py
+++ chicagoflat.py
@@ -22,11 +22,6 @@ s_sound = env.loadSoundFromFile("beep", "/menu_sounds/menu_select.wav")
 
 scene = getSceneManager()
 
-mi = ModelInfo()
-mi.name = "defaultSphere"
-mi.path = "sphere.obj"
-scene.loadModel(mi)
-
 # set background to black
 scene.setBackgroundColor(Color(0, 0, 0, 1))
 
@@ -41,7 +36,7 @@ light1.setEnabled(True)
 # Load a static osgearth 'model'
 cityModel1 = ModelInfo()
 cityModel1.name = "city1"
-cityModel1.path = "chicago_flat.map.earth"
+cityModel1.path = "chicago_yahoo.earth"
 scene.loadModel(cityModel1)
 
 # Create a scene object using the loaded model
@@ -49,26 +44,33 @@ city1 = StaticObject.create("city1")
 city1.getMaterial().setLit(False)
 
 # Load a kmz file
-searsModel = ModelInfo()
-searsModel.name = "SearsTower"
-searsModel.path = "absoluteTimezone.kmz"
-searsModel.mapName = "city1"
-scene.loadModel(searsModel)
 
-sears = StaticObject.create("SearsTower")
+loadedModels = []
+
+def loadKmz(name, filename, mapName):
+  model = ModelInfo()
+  model.name = name
+  model.path = filename
+  model.mapName = mapName
+  scene.loadModel(model)
+
+  sModel = StaticObject.create(name)
+  loadedModels.append(sModel)
+
+  return sModel
+
+absCrimes = loadKmz("absCrimes", "absoluteCrimes.kmz", "city1")
+absCrimesTime = loadKmz("absCrimesTime", "absoluteTimezone.kmz", "city1")
+#areaAround = loadKmz("areaAround", "areaAround.kmz", "city1")
+areaLess = loadKmz("areaLess", "areaLess.kmz", "city1")
+areaMore = loadKmz("areaMore", "areaMore.kmz", "city1")
+#areaEqual = loadKmz("areaEqual", "areaEqual.kmz", "city1")
+#areaZero = loadKmz("areaZero", "areaZero.kmz", "city1")
+schools = loadKmz("schools", "schools.kml", "city1")
+seriousCrimesPerTract = loadKmz("seriousCrimesPerTract", "seriousCrimesPerTract.kml", "city1")
+
 
 setNearFarZ(1, 2 * city1.getBoundRadius())
-
-# add another version with a different type of map
-cityModel2 = ModelInfo()
-cityModel2.name = "city2"
-#cityModel2.path = "chicago_flat.sat.earth"
-cityModel2.path = "chicago_yahoo.earth"
-scene.loadModel(cityModel2)
-
-# Create a scene object using the second loaded model
-city2 = StaticObject.create("city2")
-city2.getMaterial().setLit(False)
 
 #deal with the camera
 cam = getDefaultCamera()
@@ -79,11 +81,13 @@ cam.pitch(3.14159*0.45) #pitch up to start off flying over the city
 #set up the scene
 all = SceneNode.create("everything")
 all.addChild(city1)
-all.addChild(city2)
-all.addChild(sears)
+
+for model in loadedModels:
+  all.addChild(model)
+  model.setVisible(False)
 
 #turn off one of the two maps
-city1.setVisible(False)
+city1.setVisible(True)
 
 #handle events from the wand
 # left and right buttons shift between the two maps
@@ -95,7 +99,6 @@ def handleEvent():
     if(e.isButtonDown(EventFlags.ButtonLeft)):
         print("Left button pressed")
 	city1.setVisible(False)
-	city2.setVisible(True)
 
 	#play button sound
 	si_sound = SoundInstance(s_sound)
@@ -106,7 +109,6 @@ def handleEvent():
 
     if(e.isButtonDown(EventFlags.ButtonRight)):
         print("Right button pressed")
-	city2.setVisible(False)
 	city1.setVisible(True)
 
 	#play button sound
@@ -118,15 +120,38 @@ def handleEvent():
 
 # by default user can use the joystick to spin the world and fly
 # but a better flying model should be added in
-#absoluteCrimes.kmz                                                                                          100%  553KB  92.2KB/s   00:06
-#absoluteTimezone.kml                                                                                                      100% 9935KB 124.2KB/s   01:20
-#absoluteTimezone.kmz                                                                                                      100%  559KB 139.8KB/s   00:04
-#areaAround.kmz                                                                                                            100%  476KB 475.6KB/s   00:01
-#areaEqual.kmz                                                                                                             100%  114KB 113.6KB/s   00:01
-#areaLess.kmz                                                                                                              100%  411KB 137.0KB/s   00:03
-#areaMore.kmz                                                                                                              100%  135KB 135.5KB/s   00:00
-#areaZero.kmz                                                                                                              100%  983KB 491.5KB/s   00:02
-#CTABusArea.kmz                                                                                                            100% 3364KB  15.9KB/s   03:32
-#schools.kml                                                                                                               100% 1028KB  15.1KB/s   01:08
-#timezone.kmz
+
+#### MENUS
+mm = MenuManager.createAndInitialize()
+
+filters = mm.getMainMenu().addSubMenu("Filters")
+
+filtersContainer = filters.getContainer()
+filtersContainer.setLayout(ContainerLayout.LayoutHorizontal)
+f1 = Container.create(ContainerLayout.LayoutVertical, filtersContainer)
+
+#absCrimesBtn = Button.create(f1)
+#absCrimesBtn.setUIEventCommand( "showAllCrimesSelected( '%value%' )" )
+#absCrimesBtn.setText("Show Crimes")
+#absCrimesBtn.setCheckable(True)
+#absCrimes.setChecked(False)
+
+def createButton(container, kmzObj, text):
+  btn = Button.create(container)
+  btn.setUIEventCommand( "%s.setVisible(not %s.isVisible())" % (kmzObj, kmzObj))
+  #btn.setUIEventCommand( function + "()" )
+  btn.setText(text)
+  btn.setCheckable(True)
+  btn.setChecked(False)
+
+createButton(f1, "absCrimes", "Absolute Nmb of serious crimes")
+createButton(f1, "absCrimesTime", "Absolute Nmb of serious crimes per timezone")
+#createButton(f1, "areaAround", "Serious crimes compared with the surrounding area")
+#createButton(f1, "areaEqual", "Serious crimes compared with the surrounding area (Equal)")
+createButton(f1, "areaLess", "Serious crimes compared with the surrounding area (Less)")
+createButton(f1, "areaMore", "Serious crimes compared with the surrounding area (More)")
+#createButton(f1, "areaZero", "Show surrounding area")
+createButton(f1, "schools", "Schools")
+createButton(f1, "seriousCrimesPerTract", "Serious crimes per tract")
+
 setEventFunction(handleEvent)
